d569691174d7d99bc9e2566c530f52e13c2554b1
Abhay Suthar <abhaysuthar6355@gmail.com>
2024-07-07 15:28:29 +0530

    lastttttttttt

--- src/components/partials/Contact.jsx
+++ src/components/partials/Contact.jsx
@@ -3,14 +3,14 @@ import React from 'react'
 function Contact() {
   return (
     <div>
-        <h1 className='text-8xl align-center items-center text-zinc-200 mt-[20vh] mx-[65vh]'>Contact Us</h1>
+        <h1 className='text-8xl mb-10 font-bold align-center items-center text-zinc-200 mt-[20vh] mx-[65vh]'>Contact Us</h1>
         <hr className='mt-5 w-[205vh]' />
         
         <div className='text-3xl mt-10 text-zinc-200 mx-[65vh]'>
         <h3 className='mt-2'>Date Of Birth : 8/8/2004</h3>
         <h3 className='mt-2'>Email : abhaysuthar6355@gmail.com</h3>
         <h3 className='mt-2'>Address : Gandhinagar , Gujarat</h3>
-        <h3 className='mt-2'>Mobile No : 6354326242</h3>
+        <h3 className='mt-2'>Mobile No : 6354xxxxxx</h3>
     </div>
     </div>
   )
